d89e93065460f1b0ace028ec5a58057f510ae181
FrischeD <david.frischer@unifr.ch>
2021-05-14 16:24:21 +0200

    add makefile and setup.py files

---
+++ makefile
@@ -0,0 +1,6 @@
+update:
+	rm -rf build
+	rm -rf dist
+	rm -rf unifr_api_epuck.egg-info
+	python3 setup.py sdist bdist_wheel
+	python3 -m twine upload dist/*  
---
+++ setup.py
@@ -0,0 +1,35 @@
+"""Setup script for unifr_api_epuck"""
+import os.path
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the docs file
+with open(os.path.join(HERE, "readme.rst")) as fid:
+    README = fid.read()
+
+# This call to setup() does all the work
+setup(
+    name="unifr_api_epuck",
+    version="0.0.99",
+    description="An API controller for the e-puck2 from GCtronic for Webots and Python3",
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/davidfrisch/UNIFR_API_EPUCK",
+    author="David Frischer",
+    author_email="david.frischer@unifr.ch",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Pillow",
+        "smbus2",
+        "numpy"
+    ],
+)
